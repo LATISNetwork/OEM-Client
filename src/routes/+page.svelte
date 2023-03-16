da0c8af88b1b4bd6211732be23dca895a716fa61
Forrest M <forrestm@vt.edu>
2023-03-16 17:29:50 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+	import { browser } from '$app/environment';
+
+	const agent = window.electron ? 'Electron' : 'Browser';
+</script>
+
+<main>
+	<div class="m-4">
+		<h1>LATIS OEM Portal</h1>
+		<p>Running in {agent}</p>
+	</div>
+	
+
+</main>

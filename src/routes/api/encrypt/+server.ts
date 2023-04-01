f62a071af7b6f4cf34fd67ea3aa49d77ac87ac21
Forrest M <forrestm@vt.edu>
2023-03-31 20:49:40 -0400

    updated encrypt a bit

--- src/routes/api/encrypt/+server.ts
+++ src/routes/api/encrypt/+server.ts
@@ -1,28 +1,49 @@
 import fs from 'fs';
 import crypto from 'crypto';
 import type { RequestHandler } from './$types';
-export const POST = (async ({ request }) => {
-    const body = await request.json();
-    console.log('body', body);
-    const file = body.file.toString();
-    const key = crypto.scryptSync(body.key, 'salt', 32);
-    const iv = Buffer.alloc(16, 0);
-    var input = fs.createReadStream(file);
-    var output = fs.createWriteStream(file + '.enc');
-    input.pipe(crypto.createCipheriv('aes-256-cbc', key, iv)).pipe(output);
-    output.on('finish', () => {
-        console.log('done');
-    }
-    );
-    var checkSum = generateChecksum(file);
 
-    return new Response(JSON.stringify({
-        message: 'success',
-        encryptedName: file + '.enc',
-        output: checkSum,
+export const POST = (async ({ request }) => {
+  const body = await request.json();
+  console.log('body', body);
+  const file = body.file.toString();
+  const key = crypto.scryptSync(body.key.toString(), '', 32, { N: 16384, r: 8, p: 1 });
+  const iv = Buffer.alloc(16, 0);
+  console.log('key', key);
+  console.log('iv', iv);
+  const input = fs.createReadStream(file, { highWaterMark: 4096 });
+  const output = fs.createWriteStream(file + '.enc');
+  
+  const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+  input.on('data', (chunk) => {
+    const encryptedChunk = cipher.update(chunk);
+    output.write(encryptedChunk);
+  });
+  input.on('end', () => {
+    const finalEncryptedChunk = cipher.final();
+    output.write(finalEncryptedChunk, () => {
+      output.end(() => {
+        console.log('done');
+        const checkSum = fs.createReadStream(file + '.enc')
+          .pipe(crypto.createHash('md5'))
+          .digest('hex');
+        console.log('checkSum', checkSum);
+        return new Response(JSON.stringify({
+          message: 'success',
+          encryptedName: file + '.enc',
+          output: checkSum,
+        }), {
+          status: 200,
+        });
+      });
+    });
+  });
+  return new Response(JSON.stringify({
+    message: 'success',
+    encryptedName: file + '.enc',
     }), {
         status: 200,
     });
+    
 }) satisfies RequestHandler;
 
 const generateChecksum = (file:any) => {
